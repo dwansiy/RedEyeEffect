889cf9c8c75c33c270f384b6b858376a588e8540
xema0 <xema027@gmail.com>
2018-06-15 20:54:42 +0900

    Delete unused code

--- src/RedEyeEffect.java
+++ src/RedEyeEffect.java
@@ -26,17 +26,6 @@ public class RedEyeEffect {
     private static final int WINDOW_WIDTH = 600;
     private static final int WINDOW_HEIGHT = 300;
 
-    private static final int SOURCE_WIDTH = 650;
-    private static final int SOURCE_HEIGHT = 550;
-    private static final int RESULT_WIDTH = 300;
-    private static final int RESULT_HEIGHT = 300;
-
-    private static JFrame sourceJFrame;
-    private static JLabel sourceJLabel;
-
-    private static JFrame resultJFrame;
-    private static JLabel resultJLabel;
-
     static {
         System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
         eyeClassifier = new CascadeClassifier("data\\haarcascade files\\haarcascade_eye_tree_eyeglasses.xml");
@@ -147,87 +136,6 @@ public class RedEyeEffect {
         return mat;
     }
 
-    private static void processingVideo(Mat source) throws IOException {
-        Rect[] eyes = findEyes(source);
-        Mat clone = source.clone();
-        drawRectangle(clone, eyes);
-
-        BufferedImage image = convertMat(clone);
-        showOriginal(image);
-
-        if (eyes != null && eyes.length != 0) {
-            Mat resultMat = resize(new Mat(source, eyes[0]));
-
-            Mat mat = new Mat();
-            Imgproc.medianBlur(resultMat, mat, 21);
-            //Imgproc.equalizeHist(mat,mat);
-            //Imgproc.createCLAHE(5, new Size(5,5)).apply(mat,mat);
-
-            //Imgproc.threshold(mat,mat,50,255,Imgproc.THRESH_BINARY_INV);
-            //Imgproc.adaptiveTxhreshold(resultMat, mat, 255, Imgproc.ADAPTIVE_THRESH_MEAN_C, Imgproc.THRESH_BINARY_INV, 101, 1.1);
-            //Imgproc.Canny(resultMat, mat, 10, 40);
-            //Mat mask = Imgproc.getStructuringElement(Imgproc.MORPH_ELLIPSE, new Size(5, 5), new Point(1, 1));
-            //Mat mask = new Mat(new Size(3, 3), CV_8U, new Scalar(1));
-            //Mat dilated_edges = new Mat();
-            //Imgproc.morphologyEx(mat, dilated_edges, Imgproc.MORPH_CLOSE, mask, new Point(-1, -1), 1);
-            //Imgproc.dilate(mat, mat, mask, new Point(-1, -1), 4);
-            //List<MatOfPoint> contours = new ArrayList<>();
-            //Imgproc.findContours(mat, contours, new Mat(), Imgproc.RETR_LIST, Imgproc.CHAIN_APPROX_SIMPLE);
-            //for (int i = 0; i < contours.size(); i++) {
-            //    double epsilon = Imgproc.arcLength(new MatOfPoint2f(contours.get(i).toArray()), true) * 0.03;
-            //    MatOfPoint2f approx = new MatOfPoint2f();
-            //    Imgproc.approxPolyDP(new MatOfPoint2f(contours.get(i).toArray()), approx, epsilon, true);
-            //Imgproc.fillConvexPoly(mat,new MatOfPoint(approx.toArray()),new Scalar(255,255,255));
-            //List<MatOfPoint> points = new ArrayList<>();
-            //points.add(new MatOfPoint(approx.toArray()));
-            //Imgproc.polylines(mat,points,true,new Scalar(255,255,255),2);
-            //    Imgproc.drawContours(mat,contours, i, new Scalar(255, 255, 255),5);
-            //}
-
-            MatOfKeyPoint matOfKeyPoint = new MatOfKeyPoint();
-            FastFeatureDetector fastFeatureDetector = FastFeatureDetector.create();
-            fastFeatureDetector.setType(FastFeatureDetector.TYPE_9_16);
-            fastFeatureDetector.detect(mat, matOfKeyPoint);
-            //Features2d.drawKeypoints(mat, matOfKeyPoint, mat);
-
-            KeyPoint[] keyPoints = matOfKeyPoint.toArray();
-            if (keyPoints != null && keyPoints.length != 0) {
-                int x = 0;
-                int y = 0;
-                int size = keyPoints.length;
-                for (KeyPoint keyPoint : keyPoints) {
-                    x += keyPoint.pt.x;
-                    y += keyPoint.pt.y;
-                }
-                x = x / size;
-                y = y / size;
-                Point point = new Point(x, y);
-                //Imgproc.drawMarker(mat, point, new Scalar(255, 255, 255));
-            }
-
-            //std::vector < std::vector < cv::Point > >contours;
-            //cv::findContours (dilated_edges, contours, cv::RETR_LIST, cv::CHAIN_APPROX_SIMPLE);
-            //cv::approxPolyDP(cv::Mat(contours[i]), approx, cv::arcLength(cv::Mat(contours[i]), true) * 0.02, true);
-
-            BufferedImage resultBufferedImage = convertMat(mat);
-            showResult(resultBufferedImage);
-        }
-    }
-
-    public static MatOfPoint convertIndexesToPoints(MatOfPoint contour, MatOfInt indexes) {
-        int[] arrIndex = indexes.toArray();
-        Point[] arrContour = contour.toArray();
-        Point[] arrPoints = new Point[arrIndex.length];
-
-        for (int i = 0; i < arrIndex.length; i++) {
-            arrPoints[i] = arrContour[arrIndex[i]];
-        }
-
-        MatOfPoint hull = new MatOfPoint();
-        hull.fromArray(arrPoints);
-        return hull;
-    }
-
     private static BufferedImage convertMat(Mat mat) throws IOException {
         MatOfByte matOfByte = new MatOfByte();
         Imgcodecs.imencode(".jpg", mat, matOfByte);
@@ -235,26 +143,6 @@ public class RedEyeEffect {
         InputStream in = new ByteArrayInputStream(byteArray);
         return ImageIO.read(in);
     }
-
-
-    /*
-    private static BufferedImage convertMat(Mat original) {
-        BufferedImage image;
-        int width = original.width(), height = original.height(), channels = original.channels();
-        byte[] sourcePixels = new byte[width * height * channels];
-        original.get(0, 0, sourcePixels);
-
-        if (original.channels() > 1)
-            image = new BufferedImage(width, height, BufferedImage.TYPE_3BYTE_BGR);
-         else
-            image = new BufferedImage(width, height, BufferedImage.TYPE_BYTE_GRAY);
-
-        final byte[] targetPixels = ((DataBufferByte) image.getRaster().getDataBuffer()).getData();
-        System.arraycopy(sourcePixels, 0, targetPixels, 0, sourcePixels.length);
-
-        return image;
-    }
-    */
 
     private static void showMat(Mat mat, String title) {
         showMat(mat, WINDOW_WIDTH, WINDOW_HEIGHT, title);
@@ -284,43 +172,5 @@ public class RedEyeEffect {
         label = new JLabel(imageIcon);
         frame.add(label);
         frame.revalidate();
-    }
-
-    private static void initUI() {
-        sourceJFrame = new JFrame();
-        sourceJFrame.setLayout(new FlowLayout());
-        sourceJFrame.setSize(SOURCE_WIDTH, SOURCE_HEIGHT);
-        sourceJFrame.setVisible(true);
-        sourceJFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-
-        resultJFrame = new JFrame();
-        resultJFrame.setLayout(new FlowLayout());
-        resultJFrame.setSize(RESULT_WIDTH, RESULT_HEIGHT);
-        resultJFrame.setVisible(true);
-        resultJFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-    }
-
-    private static Mat resize(Mat mat) {
-        Mat result = new Mat();
-        Imgproc.resize(mat, result, new Size(RESULT_WIDTH, RESULT_HEIGHT));
-        return result;
-    }
-
-    private static void showOriginal(Image img) {
-        if (sourceJLabel != null)
-            sourceJFrame.remove(sourceJLabel);
-        ImageIcon imageIcon = new ImageIcon(img);
-        sourceJLabel = new JLabel(imageIcon);
-        sourceJFrame.add(sourceJLabel);
-        sourceJFrame.revalidate();
-    }
-
-    private static void showResult(Image img) {
-        if (resultJLabel != null)
-            resultJFrame.remove(resultJLabel);
-        ImageIcon imageIcon = new ImageIcon(img);
-        resultJLabel = new JLabel(imageIcon);
-        resultJFrame.add(resultJLabel);
-        resultJFrame.revalidate();
     }
 }
